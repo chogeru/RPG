407ac814a03ca8d36159dd0f99833f1f09308d6d
chogeru <108376848+chogeru@users.noreply.github.com>
2023-01-13 11:22:05 +0900

    1001011010

--- ゲーム/Project/GameProject/UI/Gauge.cpp
+++ ゲーム/Project/GameProject/UI/Gauge.cpp
@@ -1,6 +1,8 @@
 #include "Gauge.h"
 Gauge::Gauge(const CVector2D& p):Base(eType_UI) {
-	m_img = COPY_RESOURCE("HP", CImage);
+//	m_img = COPY_RESOURCE("HP", CImage);
+	m_img.Load("hp/UI.png");
+
 	m_pos_old = m_pos = p;
 	m_img.SetCenter(32, 32);
 	m_img.SetSize(256, 64);
@@ -14,6 +16,7 @@ void Gauge::Update()
 
 
 void Gauge::Draw() {
+
 
 	//˜g‚Ì•\Ž¦
 	//m_img.SetRect(0,0,256,64);
